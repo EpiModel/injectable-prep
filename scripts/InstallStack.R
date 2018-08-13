37012305e05eeb49e93c7f165b15e3e9f62681dd
smjenness <smjenness@gmail.com>
2018-08-13 10:48:10 -0400

    Stack install

--- scripts/InstallStack.R
+++ scripts/InstallStack.R
@@ -1,6 +1,11 @@
 
 ## Update LA PrEP stack
 
+. /suppscr/csde/sjenness/spack/share/spack/setup-env.sh
+module load r-3.5.0-gcc-8.1.0-bcqjjkd
+module load gcc-8.1.0-gcc-4.4.7-eaajvcy
+module load curl-7.60.0-gcc-8.1.0-nslvpk6
+
 # Necessary now every time using devtools on Hyak
 install.packages("curl")
 
@@ -9,7 +14,7 @@ devtools::install_github("statnet/EpiModelHPC")
 devtools::install_github("statnet/tergmLite")
 
 devtools::install_github("statnet/EpiModelHIV", ref = "injectable-prep")
-
+devtools::install_github("statnet/EpiModelHIV", ref = "syph_ept")
 
 # upload inputs
 system("scp est/*.rda hyak:/suppscr/csde/sjenness/lap/est")
