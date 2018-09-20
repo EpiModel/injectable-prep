4b50d811aec8d6c32409e261d0d2169c1ee0dceb
Samuel Jenness <smjenness@gmail.com>
2018-09-20 17:53:02 -0400

    Updated InstallStack

--- scripts/InstallStack.R
+++ scripts/InstallStack.R
@@ -1,24 +1,20 @@
 
-## Update LA PrEP stack
 
-. /suppscr/csde/sjenness/spack/share/spack/setup-env.sh
-module load r-3.5.0-gcc-8.1.0-bcqjjkd
-module load gcc-8.1.0-gcc-4.4.7-eaajvcy
-module load curl-7.60.0-gcc-8.1.0-nslvpk6
+# Install EpiModel Stack
+install.packages("EpiModel", dependencies = TRUE)
 
-# Necessary now every time using devtools on Hyak
-install.packages("curl")
+# Install Extra Helper Packages
+install.packages("devtools")
+install.packages("remotes")
 
-devtools::install_github("statnet/EpiModel")
-devtools::install_github("statnet/EpiModelHPC")
-devtools::install_github("statnet/tergmLite")
+# Latest Dev Versions of Packages
+remotes::install_github("statnet/network")
+remotes::install_github("statnet/statnet.common")
+remotes::install_github("statnet/ergm")
+remotes::install_github("statnet/tergm")
 
-devtools::install_github("statnet/EpiModelHIV", ref = "injectable-prep")
-devtools::install_github("statnet/EpiModelHIV", ref = "syph_ept")
+remotes::install_github("statnet/EpiModel")
+remotes::install_github("statnet/EpiModelHPC")
+remotes::install_github("statnet/tergmLite")
 
-# upload inputs
-system("scp est/*.rda hyak:/suppscr/csde/sjenness/lap/est")
-system("scp scripts/burnin/*.burn.* hyak:/suppscr/csde/sjenness/lap/")
-
-system("scp est/*.rda libra:~/lap/est")
-system("scp scripts/burnin/*.burn.* hyak:~/lap/")
+remotes::install_github("EpiModel/EpiModelHIV-p", ref = "injectable-prep")
