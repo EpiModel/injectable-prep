768c86d85755dc648e4c11791c24c1c7420b046f
smjenness <smjenness@gmail.com>
2018-12-06 16:56:40 -0500

    Update InstallStack.R

--- scripts/InstallStack.R
+++ scripts/InstallStack.R
@@ -4,17 +4,19 @@
 install.packages("EpiModel", dependencies = TRUE)
 
 # Install Extra Helper Packages
+install.packages("tidyverse")
 install.packages("devtools")
 install.packages("remotes")
 
 # Latest Dev Versions of Packages
-remotes::install_github("statnet/network")
-remotes::install_github("statnet/statnet.common")
-remotes::install_github("statnet/ergm")
-remotes::install_github("statnet/tergm")
+remotes::install_github(c("statnet/network",
+                          "statnet/statnet.common",
+                          "statnet/ergm",
+                          "statnet/tergm"))
 
-remotes::install_github("statnet/EpiModel")
-remotes::install_github("statnet/EpiModelHPC")
-remotes::install_github("statnet/tergmLite")
+remotes::install_github(c("statnet/EpiModel",
+                          "statnet/EpiModelHPC",
+                          "statnet/tergmLite"))
 
 remotes::install_github("EpiModel/EpiModelHIV-p", ref = "injectable-prep")
+
