7a5b5facc622c2dc04d22abd7101d273234f5323
smjenness <smjenness@gmail.com>
2018-12-06 16:57:14 -0500

    Update explore.R

--- analysis/explore.R
+++ analysis/explore.R
@@ -2,6 +2,7 @@
 ## LA PrEP Exploratory
 
 load("data/sim.n2000.rda")
+sim <- truncate_sim(sim, at = 2600)
 
 df <- as.data.frame(sim)
 names(df)
@@ -19,13 +20,17 @@ plot(sim, y = "ir100", mean.smooth = TRUE, mean.lwd = 1.25, ylim = c(0, 10))
 abline(h = 3.4, lty = 2)
 text(0, 1, round(mean(tail(df$ir100, 520)), 2))
 
-# ref and 15% LA supp
+names(df)
+plot(sim, y = "prepCurr")
+plot(sim, y = "prepCurr.la")
+
+
 
 load("data/sim.n2000.rda")
-ref <- sim
+ref <- truncate_sim(sim, at = 2600)
 
-load("data/sim.n2010.rda")
-cf <- sim
+load("data/sim.n2002.rda")
+cf <- truncate_sim(sim, at = 2600)
 
 plot(ref, y = "ir100", ylim = c(0, 6))
 abline(h = 3.4, lty = 2)
@@ -33,32 +38,23 @@ plot(cf, y = "ir100", add = TRUE,
      mean.col = "firebrick", qnts.col = "firebrick")
 
 plot(ref, y = "i.prev", ylim = c(0, 0.3))
-abline(h = 3.4, lty = 2)
+abline(h = 0.26, lty = 2)
 plot(cf, y = "i.prev", add = TRUE,
      mean.col = "firebrick", qnts.col = "firebrick")
 
 
-sims <- c(2000, 2030:2033)
-p <- wesanderson::wes_palette("Zissou1", n = length(sims), type = "continuous")
-for (j in sims) {
-  fn <- paste0("data/sim.n", j, ".rda")
-  load(fn)
-  plot(sim, y = "ir100", qnts = FALSE, ylim = c(0, 4),
-       mean.col = p[which(j == sims)],
-       add = which(j == sims) > 1)
-}
 
 
 # make master df ----------------------------------------------------------
 
-sims <- c(2000, 2030:2033)
-d <- data.frame(covo = NA, covl = NA, ir100 = NA, i.prev = NA)
+sims <- c(1000, 2000, 2002)
+d <- data.frame(sprob = NA, probi = NA, ir100 = NA, i.prev = NA)
 for (j in sims) {
   fn <- paste0("data/sim.n", j, ".rda")
   load(fn)
 
-  df <- data.frame(covo = sim$param$prep.coverage,
-                   covl = sim$param$prep.coverage.la,
+  df <- data.frame(sprob = sim$param$prep.start.prob,
+                   probi = 1-sim$param$prep.prob.oral,
                    ir100 = unname(colMeans(tail(sim$epi$ir100, 50))),
                    i.prev = as.numeric(tail(sim$epi$i.prev, 1)))
 
@@ -66,9 +62,9 @@ for (j in sims) {
 }
 d <- d[-1, ]
 str(d)
-table(d$covl)
+table(d$sprob, d$probi)
 
-p <- wesanderson::wes_palette("Zissou", n = length(unique(d$covl)), type = "continuous")
-boxplot(ir100 ~ covl, data = d, outline = FALSE, col = p, medlwd = 1.5)
-boxplot(i.prev ~ covl, data = d, outline = FALSE, col = p, medlwd = 1.5)
+p <- wesanderson::wes_palette("Zissou1", n = length(unique(d$covl)), type = "continuous")
+boxplot(ir100 ~ sprob, data = d, outline = FALSE, col = p, medlwd = 1.5)
+boxplot(i.prev ~ probi, data = d, outline = FALSE, col = p, medlwd = 1.5)
 
