9177d72f5cbb5f2202dfbd3d383ae076bc0bf2b6
maloneykevin <kevin.maloney@emory.edu>
2019-10-07 20:27:15 -0400

    Update Analysis code.R

--- analysis/Analysis code.R
+++ analysis/Analysis code.R
@@ -244,7 +244,7 @@ epi_stats(sim.base, at = 520, qnt.low = 0.025, qnt.high = 0.975)
 
 # Base scenario
 
-  load("data/sim.n2011.rda")
+  load("data/sim.n2001.rda")
   sim.base <- sim
   epi_stats(sim.base, at = 520, qnt.low = 0.025, qnt.high = 0.975)
 
@@ -406,7 +406,7 @@ epi_stats(sim.base, at = 520, qnt.low = 0.025, qnt.high = 0.975)
 
   # Base scenario
 
-  load("data/sim.n2011.rda")
+  load("data/sim.n2001.rda")
   sim.base <- sim
   epi_stats(sim.base, at = 520, qnt.low = 0.025, qnt.high = 0.975)
 
@@ -502,15 +502,15 @@ epi_stats(sim.base, at = 520, qnt.low = 0.025, qnt.high = 0.975)
     theme_classic() +
     scale_y_continuous(expand = c(0, 0)) +
     scale_x_continuous(expand = c(0, 0)) +
-    labs(y = "Proportion LAI-PrEP", x = "Probability of Initiating PrEP", fill = "PIA") +
-    scale_fill_distiller(type = "div", palette = "Greens", direction = 1,
-          breaks = c(0.0, 0.1, 0.2, 0.3, 0.4, 0.5),
-          labels = c("0%","10%","20%","30%","40%","50%")) +
+    labs(y = "Probability of choosing LAI-PrEP", x = "Probability of Initiating PrEP", fill = "PIA") +
+    scale_fill_viridis(discrete = FALSE, alpha = 1, option = "D", direction = -1,
+                       breaks = c(0.0, 0.1, 0.2, 0.3, 0.4, 0.5),
+                       labels = c("0%","10%","20%","30%","40%","50%")) +
     theme(legend.position = "right",
-          axis.text = element_text(size = 14, colour = "black"),
-          axis.title = element_text(size = 18),
-          legend.text = element_text(size = 12),
-          legend.title = element_text(size = 16),
+          axis.text = element_text(size = 9, colour = "black"),
+          axis.title = element_text(size = 11),
+          legend.text = element_text(size = 9),
+          legend.title = element_text(size = 10),
           axis.ticks.length = unit(0.25, "cm"),
           axis.ticks = element_line(color = "black"))
 
@@ -535,7 +535,7 @@ epi_stats(sim.base, at = 520, qnt.low = 0.025, qnt.high = 0.975)
 
 ### FIGURE 2 --------------------------------------------------------
 
-  load("data/sim.n2011.rda")
+  load("data/sim.n2001.rda")
   sim.base <- sim
   incid.base <- unname(colSums(sim.base$epi$incid))
 
@@ -584,20 +584,22 @@ epi_stats(sim.base, at = 520, qnt.low = 0.025, qnt.high = 0.975)
   ggplot(preds.loess2, aes(picpt, phalf)) +
     geom_raster(aes(fill = z), interpolate = TRUE) +
     geom_contour(aes(z = z), col = "white", alpha = 0.5, lwd = 0.5) +
+    geom_hline(yintercept= 5, linetype="dashed") +
+    geom_vline(xintercept = 3.59, linetype="dashed") +
     theme_classic() +
     scale_y_continuous(expand = c(0, 0)) +
     scale_x_continuous(expand = c(0, 0)) +
     labs(y = "Half-life (Weeks)", x = "Peak Plasma Concentration (ug/ml)", fill = "PIA") +
     scale_fill_viridis(discrete = FALSE, alpha = 1, option = "D", direction = -1,
-                       breaks = c(-0.05, -0.025, 0.0, 0.025),
-                       labels = c("-5%","-2.5%","0%","2.5%")) +
+                       breaks = c(0.10, 0.12, 0.14, 0.16),
+                       labels = c("10%","12%","14%","16%")) +
     theme(legend.position = "right",
-      axis.text = element_text(size = 14, colour = "black"),
-      axis.title = element_text(size = 18),
-      legend.text = element_text(size = 12),
-      legend.title = element_text(size = 16),
-      axis.ticks.length = unit(0.25, "cm"),
-      axis.ticks = element_line(color = "black"))
+          axis.text = element_text(size = 9, colour = "black"),
+          axis.title = element_text(size = 11),
+          legend.text = element_text(size = 9),
+          legend.title = element_text(size = 10),
+          axis.ticks.length = unit(0.25, "cm"),
+          axis.ticks = element_line(color = "black"))
 
   par(mar = c(3,3,2,1), mgp = c(2,1,0))
   boxplot(pia ~ picpt, data = df2, outline = FALSE, col = "orange", medlwd = 1.5, xlab = "Intercept", ylab = "PIA")
@@ -621,7 +623,7 @@ epi_stats(sim.base, at = 520, qnt.low = 0.025, qnt.high = 0.975)
 
   # Process Data
 
-  load("data/sim.n2011.rda")
+  load("data/sim.n2001.rda")
   sim.base <- sim
   incid.base <- unname(colSums(sim.base$epi$incid))
 
@@ -685,3 +687,165 @@ epi_stats(sim.base, at = 520, qnt.low = 0.025, qnt.high = 0.975)
           legend.title = element_text(size = 12),
           axis.ticks.length = unit(0.25, "cm"),
           axis.ticks = element_line(color = "black"))
+
+### ALTERNATIVE TABLE 2  --------------------------------------------------------
+  # Varying Persistence Fraction & Median Time on PrEP
+
+  # Base scenario
+
+  load("data/sim.n2001.rda")
+  sim.base <- sim
+  epi_stats(sim.base, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Counterfactual scenarios
+
+  # Median Time x 50%
+  # Fraction Low Persistent (LP) = 0%
+  load("data/sim.n6000.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 21.5%
+  load("data/sim.n6001.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 25%
+  load("data/sim.n6002.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 50%
+  load("data/sim.n6003.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 75%
+  load("data/sim.n6004.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 100%
+  load("data/sim.n6005.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Median Time x 100%
+  # Fraction Low Persistent (LP) = 0%
+  load("data/sim.n6006.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 21.5%
+  load("data/sim.n2011.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 25%
+  load("data/sim.n6008.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 50%
+  load("data/sim.n6009.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 75%
+  load("data/sim.n6010.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 100%
+  load("data/sim.n6011.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Median Time x 150%
+  # Fraction Low Persistent (LP) = 0%
+  load("data/sim.n6012.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 21.5%
+  load("data/sim.n6013.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 25%
+  load("data/sim.n6014.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 50%
+  load("data/sim.n6015.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 75%
+  load("data/sim.n6016.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 100%
+  load("data/sim.n6017.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Median Time x 200%
+  # Fraction Low Persistent (LP) = 0%
+  load("data/sim.n6018.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 21.5%
+  load("data/sim.n6019.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 25%
+  load("data/sim.n6020.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 50%
+  load("data/sim.n6021.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 75%
+  load("data/sim.n6022.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 100%
+  load("data/sim.n6023.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Median Time x 250%
+  # Fraction Low Persistent (LP) = 0%
+  load("data/sim.n6024.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 21.5%
+  load("data/sim.n6025.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 25%
+  load("data/sim.n6026.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 50%
+  load("data/sim.n6027.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 75%
+  load("data/sim.n6028.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 100%
+  load("data/sim.n6029.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Median Time x 300%
+  # Fraction Low Persistent (LP) = 0%
+  load("data/sim.n6030.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 21.5%
+  load("data/sim.n6031.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 25%
+  load("data/sim.n6032.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 50%
+  load("data/sim.n6033.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 75%
+  load("data/sim.n6034.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
+  # Fraction Low Persistent (LP) = 100%
+  load("data/sim.n6035.rda")
+  epi_stats(sim.base, sim, at = 520, qnt.low = 0.025, qnt.high = 0.975)
+
